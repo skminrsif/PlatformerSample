2e703abed048776bbf9f24dab04236c6768fffe8
skminrsif <djaleenmalabonga@gmail.com>
2020-08-14 11:10:28 -0500

    Dialogue system is now working. Just need to actually add some stuff in it.

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -5,5 +5,18 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour {
 
     public Dialogue dialogue;
-    
+
+    public void TriggerDialogue() {
+
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+
+    }
+
+    public void Start()
+    {
+        TriggerDialogue();
+
+    }
+
+
 }
