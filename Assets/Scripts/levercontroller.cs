cdbcecac6a71d885b0ba3c9717cbc9a761795068
Juliet <juliet28garcia@gmail.com>
2020-08-21 10:21:45 -0500

    added restart button

---
+++ Assets/Scripts/levercontroller.cs
@@ -0,0 +1,39 @@
+﻿using Ludiq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levercontroller : MonoBehaviour
+{
+    public GameObject Door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+   {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(Door);
+
+                }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(Door);
+
+        }
+    }
+}
+
